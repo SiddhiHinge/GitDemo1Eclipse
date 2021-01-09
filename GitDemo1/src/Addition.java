0c7b555021cc39e62ddb03f4b44e2cdc3d3a7f7f
SiddhiHinge <hingesiddhi55@gmail.com>
2021-01-09 13:07:56 +0530

    Second commit - Added Addition of 3 nos

--- GitDemo1/src/Addition.java
+++ GitDemo1/src/Addition.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class Addition {
 
 	public static void main(String[] args) {
-		int num1 , num2;
+		int num1 , num2 , num3;
 		Scanner sc = new Scanner(System.in);
+		
+		//Addition of two numbers
+		System.out.println("Addition of two numbers :=\n");
 		System.out.println("Enter first number : ");
 		num1 = sc.nextInt();
 		System.out.println("Enter second number : ");
 		num2 = sc.nextInt();
-		System.out.println("Addition of two numbers is : "+(num1+num2));
+		System.out.println("Result is : "+(num1+num2)+"\n");
+		
+		//Addition of three numbers
+		System.out.println("Addition of three numbers :=\n");
+		System.out.println("Enter first number : ");
+		num1 = sc.nextInt();
+		System.out.println("Enter second number : ");
+		num2 = sc.nextInt();
+		System.out.println("Enter third number : ");
+		num3 = sc.nextInt();
+		System.out.println("Result is : "+(num1+num2+num3)+"\n");
 	}
 
 }
